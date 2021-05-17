9e29ce4803ff83310a90c7d65a3179e57bb50926
li tong <lililililitongg@gmail.com>
2021-05-17 15:44:37 +0800

    commit

---
+++ app/src/main/java/com/example/ps04/Song.java
@@ -0,0 +1,32 @@
+package com.example.ps04;
+
+public class Song {
+    private int id;
+    private String title;
+    private String singer;
+    private int year;
+    private int stars;
+
+    public Song(int id, String title, String singer, int year, int stars) {
+        this.id = id;
+        this.title = title;
+        this.singer = singer;
+        this.year = year;
+        this.stars = stars;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getSinger() {
+        return singer;
+    }
+    public int getYear() {
+        return year;
+    }
+    public int getStars() {
+        return stars;
+    }
+}
