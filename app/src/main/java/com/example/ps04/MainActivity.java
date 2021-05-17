d9111759f4fdcb8ac1978a3671113809a10c16c4
19022187 <19022187@myrp.edu.sg>
2021-05-17 16:01:04 +0800

    linking of UI in MainActivity

--- app/src/main/java/com/example/ps04/MainActivity.java
+++ app/src/main/java/com/example/ps04/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.ps04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etTitle, etSinger, etYear;
+    Button btnInsert, btnShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etTitle = (EditText) this.findViewById(R.id.editTextTitle);
+        etSinger = (EditText) this.findViewById(R.id.editTextSinger);
+        etYear = (EditText) this.findViewById(R.id.editTextYear);
+        btnInsert = (Button) this.findViewById(R.id.buttonInsert);
+        btnShow = (Button) this.findViewById(R.id.buttonShowList);
+
     }
 }
