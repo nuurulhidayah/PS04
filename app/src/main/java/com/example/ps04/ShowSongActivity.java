295f7405b1546b8418b5a2bf00a136a6d8d51753
19022187 <19022187@myrp.edu.sg>
2021-05-17 16:43:50 +0800

    updated codes

---
+++ app/src/main/java/com/example/ps04/ShowSongActivity.java
@@ -0,0 +1,14 @@
+package com.example.ps04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowSongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_song);
+    }
+}
