fac71c0da0fc6397e3fd9c43e3cd64d235a887e1
19022187 <19022187@myrp.edu.sg>
2021-05-17 15:54:51 +0800

    did the activity_modify_song.xml

---
+++ app/src/main/java/com/example/ps04/ModifySongActivity.java
@@ -0,0 +1,14 @@
+package com.example.ps04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifySongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_song);
+    }
+}
