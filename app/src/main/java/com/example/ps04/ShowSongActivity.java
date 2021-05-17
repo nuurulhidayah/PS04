2b2a9db3e3a97879727e370114f08298a080de7c
li tong <lililililitongg@gmail.com>
2021-05-17 19:20:29 +0800

    commit

--- app/src/main/java/com/example/ps04/ShowSongActivity.java
+++ app/src/main/java/com/example/ps04/ShowSongActivity.java
@@ -3,6 +3,8 @@ package com.example.ps04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -10,6 +12,7 @@ import java.util.ArrayList;
 public class ShowSongActivity extends AppCompatActivity {
 
     ListView lv;
+    Button btnFilter;
     ArrayList<Song> aa;
     SongsArrayAdapter al;
     @Override
@@ -18,10 +21,17 @@ public class ShowSongActivity extends AppCompatActivity {
         setContentView(R.layout.activity_show_song);
 
         lv = findViewById(R.id.listViewSongs);
-
+        btnFilter = (Button) findViewById(R.id.buttonFilter);
         DBHelper db = new DBHelper(ShowSongActivity.this);
         aa = db.getAllSongs();
         al = new SongsArrayAdapter(ShowSongActivity.this, R.layout.row, aa);
         lv.setAdapter(al);
+
+        btnFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
